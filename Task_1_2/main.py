0d8dcffb295bbfd0a93db8462ff8d4784f162067
anylaim <malina23.03.2003@gmail.com>
2024-12-07 11:11:55 +1000

    Task_1_2

---
+++ Task_1_2/main.py
@@ -0,0 +1,6 @@
+tasks_done = 12
+spent_time = 1.5
+course_name = "Python"
+time_avg = spent_time/tasks_done
+
+print('Курс:', course_name, ', всего задач:', tasks_done, ', затрачено часов:', spent_time, ', среднее время выполнения', time_avg, 'часа.')
