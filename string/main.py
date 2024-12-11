e7860e33fc886776f5f8f41cbf0872f9bd8e0f38
anylaim <malina23.03.2003@gmail.com>
2024-12-11 20:14:06 +1000

    Update main.py

--- string/main.py
+++ string/main.py
@@ -2,6 +2,6 @@ example = "string for example"
 
 print(example[0])
 print(example[-1])
-print(example[(int((len(example) + 1) / 2)):])
+print(example[(int(len(example) / 2)):])
 print(example[::-1])
 print(example[1::2])
